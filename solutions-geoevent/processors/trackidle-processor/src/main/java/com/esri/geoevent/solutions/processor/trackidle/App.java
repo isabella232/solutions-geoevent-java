b18420bda473549490e8288f79ab92adbb72f8df
Patrick Hill <patrick_hill@esri.com>
2015-01-26 10:09:35 -0800

    initial commit trackidle

---
+++ solutions-geoevent/processors/trackidle-processor/src/main/java/com/esri/geoevent/solutions/processor/trackidle/App.java
@@ -0,0 +1,13 @@
+package com.esri.geoevent.solutions.processor.trackidle;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
--- solutions-geoevent/transports/webeoc-transport/src/main/java/com/esri/geoevent/solutions/transport/webeoc/WebEOCInboundTransportService.java
+++ solutions-geoevent/transports/webeoc-transport/src/main/java/com/esri/geoevent/solutions/transport/webeoc/WebEOCInboundTransportService.java
@@ -1,15 +1,19 @@
 package com.esri.geoevent.solutions.transport.webeoc;
-
 import com.esri.ges.core.component.ComponentException;
 import com.esri.ges.transport.Transport;
 import com.esri.ges.transport.TransportServiceBase;
+import com.esri.ges.transport.util.XmlTransportDefinition;
 
 public class WebEOCInboundTransportService extends TransportServiceBase {
 
-	@Override
-	public Transport createTransport() throws ComponentException {
-		// TODO Auto-generated method stub
-		return null;
-	}
+	  public WebEOCInboundTransportService()
+	  {
+	    definition = new XmlTransportDefinition(getResourceAsStream("input-transport-transport-definition.xml"));
+	  }
+	  
+	  public Transport createTransport() throws ComponentException
+	  {
+	    return new WebEOCInboundTransport(definition);
+	  }
 
 }
