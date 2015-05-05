a64c1c0602e30a9f5cbeec4f08cec90085a0cd0d
Patrick Hill <patrick_hill@esri.com>
2015-05-05 12:14:28 -0700

    bug fixes

--- solutions-geoevent/processors/timewindowsort-processor/src/main/java/com/esri/geoevent/solutions/processor/timewindowsort/TimeWindowSortProcessorDefinition.java
+++ solutions-geoevent/processors/timewindowsort-processor/src/main/java/com/esri/geoevent/solutions/processor/timewindowsort/TimeWindowSortProcessorDefinition.java
@@ -15,8 +15,38 @@ public class TimeWindowSortProcessorDefinition extends
 			.getLog(TimeWindowSortProcessorDefinition.class);
 	public TimeWindowSortProcessorDefinition() throws PropertyException
 	{
-		propertyDefinitions.put("interval", new PropertyDefinition("interval", PropertyType.Integer, 60000, "${com.esri.geoevent.solutions.processor.timewindowsort.timewindowsort-processor.LBL_INTERVAL}", "${com.esri.geoevent.solutions.processor.timewindowsort.timewindowsort-processor.DESC_INTERVAL}", false, true));
-		propertyDefinitions.put("orderby", new PropertyDefinition("interval", PropertyType.Integer, 60000, "${com.esri.geoevent.solutions.processor.timewindowsort.timewindowsort-processor.LBL_SORT_FLD}", "${com.esri.geoevent.solutions.processor.timewindowsort.timewindowsort-processor.DESC_SORT_FLD}", false, true));
+		propertyDefinitions.put("interval", new PropertyDefinition("interval", PropertyType.Integer, 60000, "${com.esri.geoevent.solutions.processor.timewindowsort.timewindowsort-processor.LBL_INTERVAL}", "${com.esri.geoevent.solutions.processor.timewindowsort.timewindowsort-processor.DESC_INTERVAL}", true, false));
+		propertyDefinitions.put("orderby", new PropertyDefinition("orderby", PropertyType.String, "", "${com.esri.geoevent.solutions.processor.timewindowsort.timewindowsort-processor.LBL_SORT_FLD}", "${com.esri.geoevent.solutions.processor.timewindowsort.timewindowsort-processor.DESC_SORT_FLD}", true, false));
+	}
+	
+	@Override
+	public String getName() {
+		return "TimeWindowSortProcessor";
+	}
+
+	@Override
+	public String getDomain() {
+		return "com.esri.geoevent.solutions.processor.timewindowsort";
+	}
+
+	@Override
+	public String getVersion() {
+		return "10.3.0";
+	}
+
+	@Override
+	public String getLabel() {
+		return "${com.esri.geoevent.solutions.processor.timewindowsort.timewindowsort-processor.PROCESSOR_LABEL}";
+	}
+
+	@Override
+	public String getDescription() {
+		return "${com.esri.geoevent.solutions.processor.timewindowsort.timewindowsort-processor.PROCESSOR_DESC}";
+	}
+
+	@Override
+	public String getContactInfo() {
+		return "geoeventprocessor@esri.com";
 	}
 
 }
