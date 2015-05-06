f0c8954a59aef38acf4108065a4a02724a034ce3
Patrick Hill <patrick_hill@esri.com>
2015-05-06 08:13:24 -0700

    bug fixes time window sort

--- solutions-geoevent/processors/timewindowsort-processor/src/main/java/com/esri/geoevent/solutions/processor/timewindowsort/TimeWindowSortProcessor.java
+++ solutions-geoevent/processors/timewindowsort-processor/src/main/java/com/esri/geoevent/solutions/processor/timewindowsort/TimeWindowSortProcessor.java
@@ -2,18 +2,22 @@ package com.esri.geoevent.solutions.processor.timewindowsort;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Observable;
+import java.util.Observer;
 import java.util.Set;
 import java.util.SortedMap;
 import java.util.TreeMap;
 
 import com.esri.ges.core.component.ComponentException;
 import com.esri.ges.core.geoevent.FieldDefinition;
+import com.esri.ges.core.geoevent.FieldException;
 import com.esri.ges.core.geoevent.FieldType;
 import com.esri.ges.core.geoevent.GeoEvent;
+import com.esri.ges.core.geoevent.GeoEventCache;
 import com.esri.ges.core.geoevent.GeoEventDefinition;
 import com.esri.ges.core.validation.ValidationException;
 import com.esri.ges.manager.geoeventdefinition.GeoEventDefinitionManager;
@@ -23,88 +27,103 @@ import com.esri.ges.messaging.GeoEventCreator;
 import com.esri.ges.messaging.GeoEventProducer;
 import com.esri.ges.messaging.Messaging;
 import com.esri.ges.messaging.MessagingException;
+import com.esri.ges.processor.CacheEnabledGeoEventProcessor;
 import com.esri.ges.processor.GeoEventProcessorBase;
 import com.esri.ges.processor.GeoEventProcessorDefinition;
 
 public class TimeWindowSortProcessor extends GeoEventProcessorBase implements
-GeoEventProducer, EventUpdatable, Runnable{
+		Runnable, GeoEventProducer, EventUpdatable {
+	private boolean monitoring = false;
+	private boolean running = false;
 	private Integer interval;
 	private String sortfield;
-	private Boolean clearCache = false;
-	private SortedMap<Object, ArrayList<GeoEvent>>sorted=new TreeMap<Object, ArrayList<GeoEvent>>();
+	private SortedMap<Object, ArrayList<GeoEvent>> sorted = new TreeMap<Object, ArrayList<GeoEvent>>();
 	private GeoEventCreator geoEventCreator;
 	private GeoEventDefinitionManager manager;
 	private Messaging messaging;
 	private GeoEventProducer geoEventProducer;
 	private long timestamp;
+	private Thread t;
+
 	public TimeWindowSortProcessor(GeoEventProcessorDefinition definition)
 			throws ComponentException {
 		super(definition);
 	}
+
 	@Override
 	public void send(GeoEvent geoEvent) throws MessagingException {
 		if (geoEventProducer != null && geoEvent != null)
 			geoEventProducer.send(geoEvent);
 	}
+
 	@Override
 	public void setId(String id) {
 		super.setId(id);
 		geoEventProducer = messaging
 				.createGeoEventProducer(new EventDestination(id + ":event"));
 	}
+
 	@Override
-	public void afterPropertiesSet()
-	{
-		interval = (Integer)properties.get("interval").getValue();
+	public void afterPropertiesSet() {
+		interval = (Integer) properties.get("interval").getValue();
 		sortfield = properties.get("orderby").getValueAsString();
 		timestamp = System.currentTimeMillis();
-		
+		this.running = true;
+
 	}
 
 	public GeoEvent process(GeoEvent evt) throws Exception {
-		if(clearCache)
-		{
-			clearCache = false;
-			Set<Object> keys = sorted.keySet();
-			Iterator<Object>it = keys.iterator();
-			while(it.hasNext())
+
+			GeoEventDefinition ged = evt.getGeoEventDefinition();
+			FieldDefinition fd = ged.getFieldDefinition(sortfield);
+			if(fd == null)
+				return null;
+			Object val = null;
+			FieldType type = fd.getType();
+			if(type == FieldType.Date)
 			{
-				Object k = it.next();
-				List<GeoEvent> list = sorted.remove(k);
-				for(GeoEvent msg: list)
-				{
-					send(msg);
-				}
-			}
-		}
-		else
-		{
-			Object fldVal = evt.getField(sortfield);
-			if(sorted.containsKey(fldVal))
-			{
-				ArrayList<GeoEvent> evtList = sorted.get(fldVal);
-				evtList.add(evt);
-				sorted.put(fldVal, evtList);
+				Date d = (Date)evt.getField(sortfield);
+				val = (Long)d.getTime();
 			}
 			else
 			{
+				val = evt.getField(sortfield);
+			}
+			if (sorted.containsKey(val)) {
+				ArrayList<GeoEvent> evtList = sorted.get(val);
+				evtList.add(evt);
+				sorted.put(val, evtList);
+			} else {
 				ArrayList<GeoEvent> evtList = new ArrayList<GeoEvent>();
 				evtList.add(evt);
-				sorted.put(fldVal,  evtList);
+				sorted.put(val, evtList);
 			}
-		}
+
 		return null;
 	}
+
 	@Override
 	public synchronized void validate() throws ValidationException {
 		// Validation Phase ...
 		super.validate();
-		
+
 	}
 	
 	@Override
+	public void onServiceStart()
+	{
+		startMonitoring();
+	}
+	
+	@Override
+	public void onServiceStop()
+	{
+		stopMonitoring();
+	}
+
+	@Override
 	public void shutdown() {
-		// Destruction Phase
+		sorted.clear();
 		super.shutdown();
 	}
 
@@ -112,7 +131,7 @@ GeoEventProducer, EventUpdatable, Runnable{
 	public boolean isGeoEventMutator() {
 		return true;
 	}
-	
+
 	@Override
 	public EventDestination getEventDestination() {
 		return (geoEventProducer != null) ? geoEventProducer
@@ -157,38 +176,69 @@ GeoEventProducer, EventUpdatable, Runnable{
 	public void update(Observable o, Object arg) {
 		;
 	}
-	
-	private SortedMap GenerateCache(GeoEvent evt)
-	{
-		SortedMap<Object, GeoEvent> sorted = null;
-		GeoEventDefinition ged = evt.getGeoEventDefinition();
-		List<FieldDefinition>fldDefs = ged.getFieldDefinitions();
-		for(FieldDefinition fd: fldDefs)
-		{
-			if(fd.getName().equals(sortfield))
-			{
-				//createCache=false;
-				return new TreeMap<Object, GeoEvent>();
-			}
-		}
-		return null;
-		
-	}
-	public void setManager(GeoEventDefinitionManager manager)
-	{
+
+	public void setManager(GeoEventDefinitionManager manager) {
 		this.manager = manager;
 	}
-	
-	public void setMessaging(Messaging messaging)
-	{
+
+	public void setMessaging(Messaging messaging) {
 		this.messaging = messaging;
 		this.geoEventCreator = messaging.createGeoEventCreator();
 	}
-	@Override
-	public void run() {
-		long now = System.currentTimeMillis();
-		
+
+	private void startMonitoring() {
+		try {
+			this.monitoring=true;
+			t = new Thread(this);
+			t.start();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 	
+	private void stopMonitoring()
+	{
+		this.monitoring=false;
+		sorted.clear();
+		t.interrupt();
+
+	}
+
+	@Override
+	public void run() {
+		while (this.monitoring) {
+			try {
+				Thread.sleep(interval);
+				if (running) {
+					long now = System.currentTimeMillis();
+					long testInterval = now - this.timestamp;
+					if (testInterval>=this.interval) {
+						timestamp = now;
+						flush();
+					}
+				}
+			} catch (Exception e) {
+
+			}
+		}
+
+	}
+	
+	private void flush() throws MessagingException
+	{
+		Set<Object> keys = sorted.keySet();
+		Iterator<Object> it = keys.iterator();
+		while (it.hasNext()) {
+			Object k = it.next();
+			List<GeoEvent> list = sorted.remove(k);
+			for (GeoEvent msg : list) {
+				send(msg);
+			}
+		}
+	}
 
 }
+	
+
+
+
